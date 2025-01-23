248d05d98be15a36bd0315739189d019a86433ba
zotel1 <zotelsigel@gmail.com>
2025-01-23 13:46:16 -0300

    first commit

---
+++ src/main/java/com/backend/fullstack/TechStoreApplication.java
@@ -0,0 +1,13 @@
+package com.backend.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/fullstack/TechStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
