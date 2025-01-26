3d218c4098262238fb01e762e0075aa288ec3a2f
zotel1 <zotelsigel@gmail.com>
2025-01-26 01:16:08 -0300

    update model Address

--- src/main/java/com/backend/fullstack/model/Address.java
+++ src/main/java/com/backend/fullstack/model/Address.java
@@ -1,4 +1,165 @@
 package com.backend.fullstack.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Addresses")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "AddressID")
+    private Long addressId;
+
+    @ManyToOne
+    @JoinColumn(name = "OrderID", nullable = false)
+    private Order order;
+
+    @Column(name = "AddressType", length = 50)
+    private String addressType;
+
+    @Column(name = "CountryCode", length = 10)
+    private String countryCode;
+
+    @Column(name = "FirsName", length = 100)
+    private String firstName;
+
+    @Column(name = "LastName", length = 100)
+    private String lastName;
+
+    @Column(name = "Company", length = 255)
+    private String company;
+
+    @Column(name = "AddressOne", length = 255)
+    private String addressOne;
+
+    @Column(name = "AddressTwo", length = 255)
+    private String addressTwo;
+
+    @Column(name = "City", length = 100)
+    private String city;
+
+    @Column(name = "StateOrProvince", length = 100)
+    private String stateOrProvince;
+
+    @Column(name = "PostalCode", length = 20)
+    private String postalCode;
+
+    @Column(name = "PhoneNumber", length = 50)
+    private String phoneNumber;
+
+    @Column(name = "EmailAddress", length = 255)
+    private String emailAddress;
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddressOne() {
+        return addressOne;
+    }
+
+    public void setAddressOne(String addressOne) {
+        this.addressOne = addressOne;
+    }
+
+    public String getAddressTwo() {
+        return addressTwo;
+    }
+
+    public void setAddressTwo(String addressTwo) {
+        this.addressTwo = addressTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStateOrProvince() {
+        return stateOrProvince;
+    }
+
+    public void setStateOrProvince(String stateOrProvince) {
+        this.stateOrProvince = stateOrProvince;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
