551a7d0dc291c4031ef68d4ce5455633e61b8497
zotel1 <zotelsigel@gmail.com>
2025-01-27 22:51:32 -0300

    add, repositories OrderRepository

---
+++ src/main/java/com/backend/fullstack/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.backend.fullstack.repositories;
+
+import com.backend.fullstack.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+    List<Order> findByOrderTypes(String orderType);
+}
